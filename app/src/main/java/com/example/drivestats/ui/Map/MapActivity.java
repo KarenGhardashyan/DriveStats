eefd36c72673812f7ae087d43d94659ffea645e5
Karen Ghardashyan <karenkrakin@gmail.com>
2024-05-17 02:01:37 +0400

    GoogleMaps

---
+++ app/src/main/java/com/example/drivestats/ui/Map/MapActivity.java
@@ -0,0 +1,45 @@
+package com.example.drivestats.ui.Map;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.drivestats.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_map);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng( 40.177200, 44.503490);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Yerevan"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12));
+
+    }
+}
--- app/src/main/java/com/example/drivestats/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/drivestats/ui/home/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.example.drivestats.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -12,8 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.drivestats.MainActivity;
 import com.example.drivestats.R;
 import com.example.drivestats.databinding.FragmentHomeBinding;
+import com.example.drivestats.ui.Map.MapActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -23,6 +27,8 @@ public class HomeFragment extends Fragment {
     private Handler handler;
     private FragmentHomeBinding binding;
 
+    private Button map;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =new ViewModelProvider(this).get(HomeViewModel.class);
@@ -31,6 +37,14 @@ public class HomeFragment extends Fragment {
 
         viewFlipper = root.findViewById(R.id.viewFlipper);
         handler = new Handler();
+        map = root.findViewById(R.id.navigationButton);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
 
         startImageSlideshow();
         return root;
